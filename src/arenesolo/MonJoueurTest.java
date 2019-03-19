9b4fe1a994c4fa4e36177c21f1d828a489ac9ca7
theomarme <x.theo63@gmail.com>
2019-03-19 16:47:58 +0100

    test getPointfromNode

--- src/arenesolo/MonJoueurTest.java
+++ src/arenesolo/MonJoueurTest.java
@@ -5,9 +5,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.awt.Point;
 
+
+
 import org.junit.jupiter.api.Test;
 
 import jeu.Joueur.Action;
+import jeu.astar.Node;
 
 class MonJoueurTest {
 
@@ -22,6 +25,15 @@ class MonJoueurTest {
 		assertEquals(Action.BAS, MonJoueur.donneDirection(p3, p1));
 		assertEquals(Action.RIEN, MonJoueur.donneDirection(p1, p1));
 		assertEquals(null, MonJoueur.donneDirection(p1, null));
+	}
+	
+	@Test
+	void testgetPointFromNode() {
+		int x=10;
+		int y=5;
+		Node monNode=new Node(x,y);
+		Point monPoint=new Point(x,y);
+		assertEquals(monPoint, MonJoueur.getPointFromNode(monNode));
 	}
 	
 	
